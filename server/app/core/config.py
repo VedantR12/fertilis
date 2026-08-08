c0959d9f04014f124c3e8c594d3f733660bb46f0
Vedant Rode <vedant.rode.2005@gmail.com>
2026-08-08 23:39:49 +0530

    done with website

--- server/app/core/config.py
+++ server/app/core/config.py
@@ -6,7 +6,6 @@ class Settings(BaseSettings):
 
     SECRET_KEY: str
     ALGORITHM: str
-    ACCESS_TOKEN_EXPIRE_MINUTES: int
 
     class Config:
         env_file = ".env"
--- server/app/core/security.py
+++ server/app/core/security.py
@@ -1,6 +1,3 @@
-from datetime import datetime, timedelta, timezone
-from typing import Optional
-
 from jose import JWTError, jwt
 from passlib.context import CryptContext
 
@@ -17,20 +14,8 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
 
-def create_access_token(
-    data: dict,
-    expires_delta: Optional[timedelta] = None,
-):
+def create_access_token(data: dict):
     to_encode = data.copy()
-
-    if expires_delta:
-        expire = datetime.now(timezone.utc) + expires_delta
-    else:
-        expire = datetime.now(timezone.utc) + timedelta(
-            minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
-        )
-
-    to_encode.update({"exp": expire})
 
     return jwt.encode(
         to_encode,
